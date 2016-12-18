0799956bc3efb91e96af37a6ccdc6bdcbaad8e97
Aydar <gareev@aydar.me>
2016-12-19 00:01:09 +0300

    add nothing text in os.args

---
+++ pushDB.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func push2Database(message string, id int) {
+	fmt.Println(message, id)
+}
--- upload.go
+++ upload.go
@@ -20,14 +20,25 @@ type ResponseSocPage struct {
 		Posts []struct {
 			Comment string `json:"comment"`
 			Num     int    `json:"num"`
+			Number  int    `json:"number"`
+			Files   []struct {
+				Path  string `json:"path"`
+				Thumb string `json:"thumbnail"`
+			} `json:"files"`
 		} `json:"posts"`
 	} `json:"threads"`
 }
 
 var url = "https://2ch.hk/soc/index.json"
+var regex string
 
 func main() {
-	regex := os.Args[1]
+	if len(os.Args) > 1 {
+		fmt.Println(len(os.Args))
+		regex = os.Args[1]
+	} else {
+		regex = ".*"
+	}
 	streamRegex := regexp.MustCompile(regex)
 	dateTread := GetThreadNumber(url)
 	threadurl := "https://2ch.hk/soc/res/" + dateTread + ".json"
@@ -36,7 +47,8 @@ func main() {
 	for number := range page.Threads[0].Posts {
 		match := streamRegex.FindStringSubmatch(page.Threads[0].Posts[number].Comment)
 		if match != nil {
-			fmt.Println(page.Threads[0].Posts[number].Comment)
+			push2Database(page.Threads[0].Posts[number].Comment, page.Threads[0].Posts[number].Num)
+			// fmt.Println(page.Threads[0].Posts[number].Files[0].Path)
 		}
 	}
 }
